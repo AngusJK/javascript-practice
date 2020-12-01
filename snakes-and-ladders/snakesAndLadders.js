a35eec31da33599603490a89714daae04a933935
Angus Kellett <anguskellett@hotmail.com>
2020-12-01 22:01:49 +0000

    adds snakeSquare and ladderSquare functions to handle landing on certain squares without repeating code

--- snakes-and-ladders/snakesAndLadders.js
+++ snakes-and-ladders/snakesAndLadders.js
@@ -5,13 +5,22 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
+/*
 const successfulRoll = function(roll, square) {
   console.log(`Y'all rolled a ${roll}. Now y'alls on square ${square}.`);
 }
 
 const tooHigh = function(roll) {
   console.log(`You rolled a ${roll}. You win!`);
+}
+*/
+
+let snakeSquare = function(x, y) {
+  console.log(`Oh, rotten luck. You rolled a ${x} and landed on a snake. You're going back to square ${y}.`);
+}
+
+let ladderSquare = function(x, y) {
+  console.log(`You rolled a ${x} and landed on a ladder! Congratulations! You're moving up to square ${y}!`);
 }
 
 rl.question("Welcome to Snakes and Ladders. Would you like to play? (Press y or n, then press enter.)\n", (answer) => {
@@ -20,35 +29,31 @@ rl.question("Welcome to Snakes and Ladders. Would you like to play? (Press y or 
     console.log("Okay bye.");
     rl.close();
   } 
-  
   else if (answer === "y") {
-    //let roll = rollDie();
-    //squareImOn += roll;
-    //rollDie(successfulRoll, tooHigh);
     const keepRollin = function() {
       rl.question("Press 'r' then 'enter' to roll\n", (answer) => {
         if (answer === "r") {
           let roll = rollDie();
           squareImOn += roll;
-
           if (squareImOn < 40) {
             if (squareImOn === 1) {
               squareImOn += 36;
-              
+              ladderSquare(roll, squareImOn);
             } else if (squareImOn === 4) {
               squareImOn += 10;
             } else if (squareImOn === 8) {
-              squareImOn += 22;
+              squareImOn -= 6;
+              snakeSquare(roll, squareImOn);
             } else if (squareImOn === 21) {
               squareImOn += 21;
+            } else {
+              console.log(`You rolled a ${roll}. You're now on square ${squareImOn}.`);
             }
-            console.log(`You rolled a ${roll}. You're now on square ${squareImOn}.`);
             keepRollin();
           } else {
             console.log(`You rolled a ${roll}. You win!`);
             rl.close();
           }
-          
         } else {
           console.log("You didn't press r. Game over.");
           rl.close();
@@ -57,14 +62,10 @@ rl.question("Welcome to Snakes and Ladders. Would you like to play? (Press y or 
     };
     keepRollin();
   } 
-  
-  
   else {
     console.log("That's not a valid answer. No soup for you!");
     rl.close();
   }
-
-
 });
 
 // node snakesAndLadders.js
