4575641178442650015739fd4a1072df88b9df90
Angus Kellett <anguskellett@hotmail.com>
2020-12-01 21:50:28 +0000

    simplifies function to only calculate roll and return the result

--- snakes-and-ladders/rollDice.js
+++ snakes-and-ladders/rollDice.js
@@ -1,17 +1,28 @@
+/*
 let squareImOn = 0;
-const rollDie = function(callback) {
+const rollDie = function(rollAgainCallback, gameOverCallback) {
   let result = Math.floor(Math.random() * 6) + 1;
   squareImOn += result;
-  if (squareImOn === 1) {
-    squareImOn += 36;
-  } else if (squareImOn === 4) {
-    squareImOn += 10;
-  } else if (squareImOn === 8) {
-    squareImOn += 22;
-  } else if (squareImOn === 21) {
-    squareImOn += 21;
+  if (squareImOn < 40) {
+    if (squareImOn === 1) {
+      squareImOn += 36;
+    } else if (squareImOn === 4) {
+      squareImOn += 10;
+    } else if (squareImOn === 8) {
+      squareImOn += 22;
+    } else if (squareImOn === 21) {
+      squareImOn += 21;
+    }
+    rollAgainCallback(result, squareImOn);
+  } else {
+    gameOverCallback(result);
   }
-  callback(result, squareImOn);
+};
+*/
+
+const rollDie = function() {
+  let result = Math.floor(Math.random() * 6) + 1;
+  return result;
 };
 
 module.exports = rollDie;
