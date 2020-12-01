eac115c9e3adc5f7f51dfb0c783dff31c638a2af
Angus Kellett <anguskellett@hotmail.com>
2020-12-01 21:08:25 +0000

    passes in successfulRoll and tooHigh functions to deal with the event that squareImOn exceeds 100

--- snakes-and-ladders/snakesAndLadders.js
+++ snakes-and-ladders/snakesAndLadders.js
@@ -6,20 +6,24 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+const successfulRoll = function(roll, square) {
+  console.log(`Y'all rolled a ${roll}. Now y'alls on square ${square}.`);
+}
+
+const tooHigh = function(roll) {
+  console.log(`You rolled a ${roll}. You win!`);
+}
+
 rl.question("Welcome to Snakes and Ladders. Would you like to play? (Press y or n, then press enter.)\n", (answer) => {
   if (answer === "n") {
     console.log("Okay bye.");
     rl.close();
   } else if (answer === "y") {
-    rollDie((x,y) => {
-      console.log(`Y'all rolled a ${x}. Now y'alls on square ${y}.`);
-    });
+    rollDie(successfulRoll, tooHigh);
     const keepRollin = function() {
       rl.question("Press r then enter to roll again\n", (answer) => {
         if (answer === "r") {
-          rollDie((x,y) => {
-            console.log(`Y'all rolled a ${x}. Now y'alls on square ${y}.`);
-          });
+          rollDie(successfulRoll, tooHigh);
           keepRollin();
         } else {
           console.log("You didn't press r. Game over.");
