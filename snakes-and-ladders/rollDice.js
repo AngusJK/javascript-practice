7a89c499db08b3bcd52a072c8c9121144db6a34e
Angus Kellett <anguskellett@hotmail.com>
2020-12-02 02:45:54 +0000

    removes commented-out code

--- snakes-and-ladders/rollDice.js
+++ snakes-and-ladders/rollDice.js
@@ -1,28 +1,6 @@
-/*
-let squareImOn = 0;
-const rollDie = function(rollAgainCallback, gameOverCallback) {
-  let result = Math.floor(Math.random() * 6) + 1;
-  squareImOn += result;
-  if (squareImOn < 40) {
-    if (squareImOn === 1) {
-      squareImOn += 36;
-    } else if (squareImOn === 4) {
-      squareImOn += 10;
-    } else if (squareImOn === 8) {
-      squareImOn += 22;
-    } else if (squareImOn === 21) {
-      squareImOn += 21;
-    }
-    rollAgainCallback(result, squareImOn);
-  } else {
-    gameOverCallback(result);
-  }
-};
-*/
-
-const rollDie = function() {
+const rollDice = function() {
   let result = Math.floor(Math.random() * 6) + 1;
   return result;
 };
 
-module.exports = rollDie;
+module.exports = rollDice;
